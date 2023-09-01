5574ba1f0b61bc5ada193ce457bb4184feb43b08
NurMukashev <mukashev.nur@gmail.com>
2023-09-01 15:21:21 +0600

    кастомизировал create

--- resources/js/Pages/Documents/Create.vue
+++ resources/js/Pages/Documents/Create.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { Head } from '@inertiajs/vue3'
+import { Link } from "@inertiajs/vue3"
 import { useForm } from '@inertiajs/vue3'
 
 const form = useForm({
@@ -18,13 +19,20 @@ function submit() {
 <template>
 
     <Head title="Create" />
-    <main class="mx-auto p-2">
+    <main class="w-1/3 mx-auto p-2">
 
-        <h2 class="text-lg text-center m-4">Создание документа (Файла)</h2>
+        <div class="mx-auto pt-6 my-6">
+            <h2 class="text-xl text-center font-bold">Создание документа (Файла)</h2>
+        </div>
+        <div class="mx-auto mb-2 text-orange-600 hover:underline">
+            <Link :href="route('documents.index')">Назад</Link>
+        </div>
 
-        <form @submit.prevent="submit" class="mx-auto w-1/3 flex flex-col justify-around items-center">
+        <p class="text-sm text-left">*Все поля являются обязательными</p>
+        <p class="text-sm text-left">*Размер файла не должен превышать 8 мб</p>
+        <form @submit.prevent="submit" class="mx-auto flex flex-col justify-around bg-gray-200 p-8">
                 <div class="w-full mb-3">
-                    <input v-model="form.name" type="text" placeholder="Название" autofocus>
+                    <input v-model="form.name" type="text" placeholder="Название" class="w-full" autofocus>
                     <div v-if="errors.name" class="text-red-500">{{ errors.name }}</div>
                 </div>
                 <div class="w-full mb-3">
@@ -36,8 +44,8 @@ function submit() {
                         {{ form.progress.percentage }}%
                     </progress>
                 </div>
-                <div class="w-full mb-3">
-                    <button type="submit" class="px-2 py-2 bg-orange-500 hover:bg-orange-700 cursor-pointer text-md">Создать</button>
+                <div class="w-full mb-3 text-right">
+                    <button type="submit" class="ml-auto px-4 py-2 bg-orange-500 hover:bg-orange-700 cursor-pointer text-md font-bold">Создать</button>
                 </div>
         </form>
 
