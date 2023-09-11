8ed743117c5cd06fcbd6c54ad7a3f1dedbf35651
NurMukashev <mukashev.nur@gmail.com>
2023-09-11 13:37:01 +0600

    Отобразил кропнутое изображение и кастомизировал главную страницу

--- resources/js/Pages/Documents/Index.vue
+++ resources/js/Pages/Documents/Index.vue
@@ -13,15 +13,6 @@ import { Head } from "@inertiajs/vue3";
         }
     }
 
-    /*function submit(id) {
-        alert(1);
-        router.post(`/documents/${id}`, {
-            _method: 'destroy',
-           // name: form.name,
-            //file: form.file
-        })
-    }*/
-
 </script>
 
 <template>
@@ -34,26 +25,34 @@ import { Head } from "@inertiajs/vue3";
         </div>
         <div v-if="message" class="text-green-600">{{ message }}</div>
         <div class="w-3/4 mx-auto text-right mb-4">
+            <p class="mb-2">Общее количество записей: {{ documents.length }}</p>
             <Link :href="route('documents.create')" class="px-4 py-2 bg-orange-500 hover:bg-orange-700 cursor-pointer text-md font-bold">Создать файл</Link>
         </div>
         <table class="w-3/4 mx-auto text-md table-auto border border-gray-300">
             <thead class="bg-orange-300">
-            <tr>
-                <th class="border border-gray-300 px-6 py-4">Название</th>
-                <th class="border border-gray-300 px-6 py-4">Имя файла</th>
-                <th class="border border-gray-300 px-6 py-4">Манипуляции</th>
-            </tr>
+                <tr>
+                    <th class="border border-gray-300 px-6 py-4">Название</th>
+                    <th class="border border-gray-300 px-6 py-4">Имя файла</th>
+                    <th class="border border-gray-300 px-6 py-4">Расширение</th>
+                    <th class="border border-gray-300 px-6 py-4">Манипуляции</th>
+                </tr>
             </thead>
             <tbody>
-            <tr v-for="document in documents">
-                <td class="border border-gray-300 px-6 py-4">{{ document.name }}</td>
-                <td class="border border-gray-300 px-6 py-4">{{ document.file_name }}</td>
-                <td class="border border-gray-300 px-6 py-4">
-                    <Link :href="route('documents.edit', document.id)" class="mx-1 px-3 py-2 bg-orange-500 hover:bg-orange-700 cursor-pointer text-md" >Изменить</Link>
-                    <button @click="destroy(document.id)" class="mx-1 px-3 py-2 bg-red-500 hover:bg-red-700 cursor-pointer text-md">Удалить</button>
-                    <a :href="'download/'+ document.id" class="mx-1 px-3 py-2 bg-red-500 hover:bg-red-700 cursor-pointer text-md">Скачать</a>
-                </td>
-            </tr>
+                <tr v-for="document in documents">
+                    <td class="border border-gray-300 px-6 py-4">{{ document.name }}</td>
+                    <td class="border border-gray-300 px-6 py-4">{{ document.file_name }}</td>
+                    <td class="border border-gray-300 px-6 py-4  text-center">
+                        {{ document.extension }}
+                        <div v-if="document.crop_path" class="mt-2">
+                            <img :src="document.crop_path" :alt="'image_' + document.id">
+                        </div>
+                    </td>
+                    <td class="border border-gray-300 px-1 py-2 flex flex-col text-center">
+                        <Link :href="route('documents.edit', document.id)" class="mx-1 my-1 px-2 py-1 bg-orange-500 hover:bg-orange-700 cursor-pointer text-md" >изменить</Link>
+                        <button @click="destroy(document.id)" class="mx-1 my-1 px-2 py-1 bg-red-500 hover:bg-red-700 cursor-pointer text-md">удалить</button>
+                        <a :href="'download/'+ document.id" class="mx-1 my-1 px-2 py-1 bg-red-500 hover:bg-red-700 cursor-pointer text-md">скачать</a>
+                    </td>
+                </tr>
             </tbody>
         </table>
     </main>
